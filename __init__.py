586e4ee0723958d025c2a877cb97c67ba35b2e5f
billyrrr <billrao@me.com>
2019-08-23 13:00:26 -0700

    Fix __all__

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .src import config, context, auth, fields, schema, serializable, \
-    firestore_object, domain_model, view_model
+    firestore_object, domain_model, view_model, view
 
 __all__ = ["config", "context", "auth", "fields", "schema", "serializable",
-           "firestore_object", "domain_model", "view_model"]
+           "firestore_object", "domain_model", "view_model", "view"]
