f19012063bbc81614578fd413b7c64f5ad5f8a88
billyrrr <billrao@me.com>
2019-09-01 15:27:30 -0700

    Bump up version to 0.0.1a4

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="flask_boiler",
     # Alpha release
-    version="0.0.1a3",
+    version="0.0.1a4",
     author="Bill Rao",
     author_email="billrao@me.com",
     description="Build flask project with Firebase ",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/billyrrr/flask-boiler",
-    download_url="https://github.com/billyrrr/flask-boiler/archive/v0.0.1a3"
+    download_url="https://github.com/billyrrr/flask-boiler/archive/v0.0.1a4"
                  ".tar.gz",
     keywords=["firebase", "firestore", "ORM", "flasgger", "flask",
               "backend", "nosql"],
