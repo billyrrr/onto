7346f0c94945adf4d41f7576e09a7732bbf1d9bd
billyrrr <billrao@me.com>
2019-09-14 17:06:34 -0700

    Improve query

--- flask_boiler/query_mixin.py
+++ flask_boiler/query_mixin.py
@@ -1,4 +1,4 @@
-from google.cloud.firestore import DocumentSnapshot, CollectionReference
+from google.cloud.firestore import DocumentSnapshot, CollectionReference, Query
 
 from flask_boiler.firestore_object import snapshot_to_obj
 
@@ -33,23 +33,10 @@ class QueryMixin:
             obj._import_properties(doc.to_dict())
             yield obj
 
-    @classmethod
-    @convert_query_ref
-    def where(cls, *args, **kwargs):
-        """ Note that indexes may need to be added from the link provided
-                by firestore in the error messages
-
-        TODO: add error handling and argument checking
-
-        :param args:
-        :param kwargs:
-        :return:
-        """
-
-        if len(args) == 0 and len(kwargs) == 0:
-            raise ValueError("Empty where")
-
-        cur_where = cls._get_collection()
+    @staticmethod
+    def _append_original(*args, cur_where=None) -> Query:
+        if cur_where is None:
+            raise ValueError
 
         if len(args) != 0:
             if len(args) % 3 != 0:
@@ -64,10 +51,60 @@ class QueryMixin:
                         arg_stack.pop(0),
                         arg_stack.pop(0)
                     )
+        return cur_where
+
+    @classmethod
+    def _append_new_style(cls, *, cur_where=None, **kwargs):
+        if cur_where is None:
+            raise ValueError
 
         for key, val in kwargs.items():
             comp, other = val if isinstance(val, tuple) else ("==", val)
             firestore_key = cls.get_schema_cls().f(key)
             cur_where = cur_where.where(firestore_key, comp, other)
+        return cur_where
+
+    @classmethod
+    def _where_query(cls, *args, cur_where=None, **kwargs):
+        if len(args) == 0 and len(kwargs) == 0:
+            raise ValueError("Empty where")
+        if cur_where is None:
+            raise ValueError
+
+        cur_where = cls._append_original(*args, cur_where=cur_where)
+        cur_where = cls._append_new_style(**kwargs, cur_where=cur_where)
+        return cur_where
+
+    @classmethod
+    @convert_query_ref
+    def where(cls, *args,
+              acsending=None,
+              descending=None,
+              end_at=None,
+              end_before=None,
+              limit=None,
+              offset=None,
+              order_by=None,
+              select=None,
+              start_after=None,
+              start_at=None,
+              **kwargs,):
+        """ Note that indexes may need to be added from the link provided
+                by firestore in the error messages
+
+        TODO: add error handling and argument checking
+        TODO: implement limit, orderby, etc.
+
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+        cur_where = Query(parent=cls._get_collection(),
+                          # TODO: pass caught kwargs to Query constructor
+                          )
+
+        cur_where = cls._where_query(*args, **kwargs,
+                                     cur_where=cur_where)
 
         return cur_where
